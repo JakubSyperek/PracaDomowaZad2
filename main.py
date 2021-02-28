504eaead950aa1eb186d65bea3124d4fa5b62dca
JakubSyperek <162420@student.uwm.edu.pl>
2021-02-28 22:06:00 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+a=2
+b=3
+
+dodawanie = a + b
+print("a + b =", dodawanie)
+odejmowanie = a - b
+print("a - b =", odejmowanie)
+mnozenie = a * b
+print("a * b =", mnozenie)
+dzielenie = a / b
+print("a / b =", dzielenie)
+
+
+
